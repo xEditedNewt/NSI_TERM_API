e31f4df5f0261c6cccfeff4a440c974472466af9
Irae <antoine.lutfalla@gmail.com>
2023-02-08 08:34:18 +0100

    Add files via upload

---
+++ acceuil/main.js
@@ -0,0 +1,94 @@
+(function($) { "use strict";
+var id = localStorage.getItem("id");
+  var app = function () {
+      var body = undefined;
+      var menu = undefined;
+      var menuItems = undefined;
+      var init = function init() {
+          body = document.querySelector('body');
+          menu = document.querySelector('.menu-icon');
+          menuItems = document.querySelectorAll('.nav__list-item');
+          applyListeners();
+      };
+      var applyListeners = function applyListeners() {
+          menu.addEventListener('click', function () {
+              return toggleClass(body, 'nav-active');
+          });
+      };
+      var toggleClass = function toggleClass(element, stringClass) {
+          if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+      };
+      init();
+  }();        
+            
+})(jQuery); 
+
+
+function table_conversation(array) {
+    const table = document.querySelector('.table-conversation');
+  
+    array.forEach(item => {
+        console.log(item)
+      const tr = document.createElement('tr');
+      const td1 = document.createElement('td');
+      const td2 = document.createElement('td');
+      const td3 = document.createElement('td');
+      const td4 = document.createElement('td');
+    
+      td1.textContent = item.user;
+      td2.textContent = item.email;
+      td3.textContent = item.lastMessage;
+      td4.innerHTML = `<a onclick="href_conv(${item.idConv})" class="noir petit arrondi">Voir</a>`
+
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      table.appendChild(tr);
+    });
+  }
+
+async function verification_conversation(){
+    var id = localStorage.getItem("id");
+    var table = document.getElementById('haut');
+    fetch("http://45.145.166.47:5000/api/v1/conversations/", {
+		method: 'GET',
+		headers: {
+			id:id,
+		}
+	  }).then(res => res.json()).then(data =>{
+        if (data.message == ' '){
+            console.log("test")
+            table.style.display = "block",
+            table.style.text-align = "center";
+            table_conversation(data.data)
+        }
+        else{
+            var message = document.getElementById("message");
+            message.style.display = "block";
+            message.innerHTML = data.message;
+        }
+    })
+}
+
+
+verification_conversation()
+function href_conv(id_conv){
+    window.location.assign('./chat/index.html?id=' + id_conv);
+
+}
+
+function logout(){
+    localStorage.setItem("code", " ");
+    localStorage.setItem("id", " ")
+    localStorage.setItem("token", " ")
+    localStorage.setItem("username", " ")
+    window.location.href = '../index.html'
+}
+function check_page(){
+    id = localStorage.getItem("id")
+    if (id == " "){
+        window.location.href = '../index.html'
+    }
+}
+check_page()
---
+++ main.js
@@ -0,0 +1,151 @@
+
+async function creation_compte(){
+	var email = document.getElementById("inputEmail").value;
+    var password = document.getElementById("password").value;
+	var name = document.getElementById("name").value;
+	fetch('http://45.145.166.47:5000/api/v1/creation/', {
+		method: 'POST',
+		headers: {
+			email:email,
+			mdp:password,
+			name:name
+		}
+	  })
+		.then(res => res.json())
+		.then(data => {
+			document.getElementById("error").style.display = "none";
+		document.getElementById("succes").style.display = "block";
+        document.getElementById("succes").innerHTML = data.message;
+        localStorage.setItem("id", data.id);
+        localStorage.setItem("username", data.name);
+        localStorage.setItem("email", email);
+        window.location.href = 'acceuil/index.html'
+		})
+		.catch(err => {
+		  console.error(err);
+		});
+}
+async function connection(){
+	var email = document.getElementById("inputEmail1").value;
+    var password = document.getElementById("password1").value;
+	fetch('http://45.145.166.47:5000/api/v1/verification/', {
+		method: 'GET',
+		headers: {
+			email:email,
+			mdp:password
+		}
+	  })
+		.then(res => res.json())
+		.then(data => {
+		  document.getElementById("error1").style.display = "block";
+		  document.getElementById("error1").innerHTML = data.message;
+		  if(data.message == ' '){
+			localStorage.setItem("token", data.token);
+			localStorage.setItem("username", data.name);
+            localStorage.setItem("email", email);
+			document.getElementById("error1").innerHTML = "Vous êtes connecté";
+            localStorage.setItem("id", data.id);
+            window.location.href = 'acceuil/index.html'
+		  }
+		})
+		.catch(err => {
+		  console.error(err);
+	});
+}
+
+
+function checkInputs_CreationCompte() {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("inputEmail").value;
+	var password = document.getElementById("password").value;
+	var errorDiv = document.getElementById("error");
+	var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+	if (name && emailRegex.test(email) && password) {
+	  errorDiv.style.display = "none";
+	  verificationmail(email);
+	} else {
+	  errorDiv.style.display = "block";
+	  errorDiv.innerHTML = "Veuillez remplir tous les champs correctement";
+	}
+  }
+
+
+function checkInputs_Connection() {
+	var email = document.getElementById("inputEmail1").value;
+	var password = document.getElementById("password1").value;
+	var errorDiv = document.getElementById("error1");
+	var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+	if (emailRegex.test(email) && password) {
+	  errorDiv.style.display = "none";
+	  connection();
+	} else {
+		if (!emailRegex.test(email)){
+			errorDiv.style.display = "block";
+			errorDiv.innerHTML = "Veuillez entrer une adresse mail valide";
+		}
+		else {
+			errorDiv.style.display = "block";
+			errorDiv.innerHTML = "Veuillez remplir tous les champs";
+		}
+	}
+  }
+
+async function verificationcode(){
+	var boutonverification = document.getElementById("boutonverification")
+	var boutonCreation = document.getElementById("boutonCreation")
+	boutonCreation.style.display = "none"
+	boutonverification.style.display = "block"
+	var mail = document.getElementById("inputEmail").value;
+	var name = document.getElementById("name");
+	var email = document.getElementById("inputEmail");
+	var password = document.getElementById("password");
+	var codeEnter = document.getElementById("code1")
+	codeEnter.style.display = "block"
+	name.style.display = "none"
+	email.style.display = "none"
+	password.style.display = "none"
+	code = [...Array(6)].map(()=>Math.floor(Math.random()*10)).join('')
+    console.log(code)
+	localStorage.setItem("code", code);
+   Email.send({
+      SecureToken : "7a520917-fffb-441a-9ad4-625da6b2319c",
+      To : mail.toString(),
+      From : "azute.noie@gmail.com",
+      Subject : "Code vérification",
+      Body : code
+   }).then(
+   message => alert("Le code de vérification vous a été envoyé par mail (verifiez vos spam)")
+   );
+}
+async function checkCode(){
+	var errorDiv = document.getElementById("error");
+	var codeEnter = document.getElementById("code1").value
+	var code = localStorage.getItem("code");
+	if(code == codeEnter){
+		creation_compte()
+	}
+	else{
+		errorDiv.style.display = "block";
+		errorDiv.innerHTML = "Mauvais code, veuillez re tenter";
+	}
+}
+
+async function verificationmail(email){
+	fetch('http://45.145.166.47:5000/api/v1/email/' + email)
+		.then(res => res.json())
+		.then(data => {
+		  if(data.message == ' '){
+			verificationcode();
+		  }
+		  else{
+			var errorDiv = document.getElementById("error");
+			errorDiv.style.display = "block";
+			errorDiv.innerHTML = data.message;
+		  }
+		})
+		.catch(err => {
+		  console.error(err);
+		});
+}
